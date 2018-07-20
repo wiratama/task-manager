ef158069ec98d8c29f44ec93bcb6fb1bbe266c25
Arya Wiratama <aryawiratama99@gmail.com>
2018-07-21 03:16:24 +0800

    app base assets disable precompile assets load static assets

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module TaskManager
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # turn assets pipeline off
+    config.assets.enabled = false
   end
 end
